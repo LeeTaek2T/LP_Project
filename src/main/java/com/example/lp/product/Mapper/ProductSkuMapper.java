c03b7d7e6d96ff1a473b0b88231383f44951f824
LeeTaek2T <fshioncafe@naver.com>
2025-08-24 20:24:31 +0900

    feat : Product, ProductSku Mapper 개발 완료

---
+++ src/main/java/com/example/lp/product/Mapper/ProductSkuMapper.java
@@ -0,0 +1,24 @@
+package com.example.lp.product.Mapper;
+
+import com.example.lp.product.dto.request.ProductSkuRequest;
+import com.example.lp.product.dto.response.ProductSkuResponse;
+import com.example.lp.product.entity.Product;
+import com.example.lp.product.entity.ProductSku;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ProductSkuMapper {
+
+    public ProductSku requestToEntity(ProductSkuRequest productSkuRequest, Product product){
+        return new ProductSku(product, productSkuRequest.skuCode(), productSkuRequest.color(), productSkuRequest.size(),
+                productSkuRequest.stock(), productSkuRequest.price(), productSkuRequest.inActive(),
+                LocalDateTime.now(), LocalDateTime.now());
+    }
+
+    public ProductSkuResponse entityToResponse(ProductSku productSku){
+        return new ProductSkuResponse(productSku.getId(), productSku.getProduct().getId(),productSku.getSkuCode(), productSku.getColor(),
+                productSku.getSize(), productSku.getStock(), productSku.getPrice(), productSku.getInActive());
+    }
+}
