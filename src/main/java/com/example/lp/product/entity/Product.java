83ef6a7c5012cc81e28c111552d8a0717b80a85c
LeeTaek2T <fshioncafe@naver.com>
2025-08-24 20:24:17 +0900

    feat : Product, ProductSku 엔터티 개발 완료

---
+++ src/main/java/com/example/lp/product/entity/Product.java
@@ -0,0 +1,49 @@
+package com.example.lp.product.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="product_id")
+    private Long id;
+
+    @Column(name="name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name="brand", length = 255, nullable = false)
+    private String brand;
+
+    @Column(name="created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductSku> productSkuList = new ArrayList<>();
+
+
+    public Product(){}
+
+    public Product(String name, String brand, LocalDateTime createdAt) {
+        this.brand = brand;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+}
---
+++ src/main/java/com/example/lp/product/entity/ProductSku.java
@@ -0,0 +1,89 @@
+package com.example.lp.product.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "productSku")
+public class ProductSku {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sku_id")
+    private Long id;
+
+    @Column(name = "sku_code", length = 100, nullable = false)
+    private String skuCode;
+
+    @Column(name = "color", length = 5, nullable = false)
+    private String color;
+
+    @Column(name = "size", length = 5, nullable = false)
+    private String size;
+
+    @Column(name = "stock", nullable = false)
+    private int stock;
+
+    @Column(name = "price",nullable = false)
+    private int price;
+
+    @Column(name = "in_active", nullable = false)
+    private Boolean inActive;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public ProductSku(){}
+
+    public ProductSku(Product product, String skuCode, String color, String size, int stock, int price,
+                      Boolean inActive, LocalDateTime createdAt, LocalDateTime updatedAt){
+        this.product = product;
+        this.skuCode = skuCode;
+        this.color = color;
+        this.size = size;
+        this.stock = stock;
+        this.price = price;
+        this.inActive = inActive;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSkuCode() {
+        return skuCode;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Boolean getInActive() {
+        return inActive;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+}
