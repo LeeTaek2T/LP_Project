62539de4867039d2392209f1a19a1832560673b5
LeeTaek2T <fshioncafe@naver.com>
2025-08-24 20:23:32 +0900

    feat : ProductService, ProductSkuService 모든 기능 개발 완료

---
+++ src/main/java/com/example/lp/product/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.example.lp.product.service;
+
+import com.example.lp.product.Mapper.ProductMapper;
+import com.example.lp.product.dto.request.ProductRequest;
+import com.example.lp.product.dto.response.ProductResponse;
+import com.example.lp.product.entity.Product;
+import com.example.lp.product.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    public ProductService(ProductRepository productRepository,
+                          ProductMapper productMapper){
+        this.productRepository=productRepository;
+        this.productMapper=productMapper;
+    }
+
+    public Long registerProduct(ProductRequest productRequest){
+        Product product = productMapper.requestToEntity(productRequest);
+        Product savedProduct = productRepository.save(product);
+        return savedProduct.getId();
+    }
+
+    public List<ProductResponse> getAllProduct() {
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponse> productRequestList = convertToProductResponseList(productList);
+        return productRequestList;
+    }
+
+    private List<ProductResponse> convertToProductResponseList(List<Product> productList){
+        List<ProductResponse> productResponseList = new ArrayList<>();
+        for(Product product : productList){
+            ProductResponse productResponse = productMapper.entityToResponse(product);
+            productResponseList.add(productResponse);
+        }
+        return productResponseList;
+    }
+}
---
+++ src/main/java/com/example/lp/product/service/ProductSkuService.java
@@ -0,0 +1,49 @@
+package com.example.lp.product.service;
+
+import com.example.lp.product.Mapper.ProductSkuMapper;
+import com.example.lp.product.dto.request.ProductSkuRequest;
+import com.example.lp.product.dto.response.ProductSkuResponse;
+import com.example.lp.product.entity.Product;
+import com.example.lp.product.entity.ProductSku;
+import com.example.lp.product.repository.ProductRepository;
+import com.example.lp.product.repository.ProductSkuRepository;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class ProductSkuService {
+
+    private final ProductSkuRepository productSkuRepository;
+    private final ProductRepository productRepository;
+    private final ProductSkuMapper productSkuMapper;
+
+    public ProductSkuService(ProductSkuRepository productSkuRepository, ProductSkuMapper productSkuMapper,
+                             ProductRepository productRepository){
+        this.productSkuRepository = productSkuRepository;
+        this.productRepository = productRepository;
+        this.productSkuMapper = productSkuMapper;
+    }
+
+    public Long registerProductSku(ProductSkuRequest productSkuRequest){
+        Product product = productRepository.findById(productSkuRequest.productId())
+                .orElseThrow(() -> new RuntimeException(""));
+        ProductSku productSku = productSkuMapper.requestToEntity(productSkuRequest, product);
+        ProductSku registerdProductSku = productSkuRepository.save(productSku);
+        return registerdProductSku.getId();
+    }
+
+    public List<ProductSkuResponse> getAllProductSku(Long productId){
+        List<ProductSku> productSkuList = productSkuRepository.findAllByProductId(productId);
+        List<ProductSkuResponse> productSkuResponseList = convertToProductSkuResponseList(productSkuList);
+        return productSkuResponseList;
+    }
+
+    private List<ProductSkuResponse> convertToProductSkuResponseList(List<ProductSku> productSkuList){
+        List<ProductSkuResponse> productSkuResponseList = new ArrayList<>();
+        for(ProductSku productSku : productSkuList){
+            ProductSkuResponse productSkuResponse = productSkuMapper.entityToResponse(productSku);
+            productSkuResponseList.add(productSkuResponse);
+        }
+        return productSkuResponseList;
+    }
+}
