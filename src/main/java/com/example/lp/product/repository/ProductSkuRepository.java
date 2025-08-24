f4f3010b5e1774a41d46797cf1b9b575dc229870
LeeTaek2T <fshioncafe@naver.com>
2025-08-24 20:23:52 +0900

    feat : ProductRepository, ProductSkuRepository 모든 기능 개발 완료

---
+++ src/main/java/com/example/lp/product/repository/ProductSkuRepository.java
@@ -0,0 +1,11 @@
+package com.example.lp.product.repository;
+
+import com.example.lp.product.entity.ProductSku;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+@Repository
+public interface ProductSkuRepository extends JpaRepository<ProductSku, Long> {
+    List<ProductSku> findAllByProductId(Long productId);
+}
