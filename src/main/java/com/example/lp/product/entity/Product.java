6392095fa7db5ea1edc99ca09e95684858ba8bd2
LeeTaek2T <fshioncafe@naver.com>
2025-09-12 00:05:18 +0900

    feat : Product Entity수정

--- src/main/java/com/example/lp/product/entity/Product.java
+++ src/main/java/com/example/lp/product/entity/Product.java
@@ -2,6 +2,7 @@ package com.example.lp.product.entity;
 
 import jakarta.persistence.*;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDateTime;
@@ -17,15 +18,27 @@ public class Product {
     @Column(name="name", length = 255, nullable = false)
     private String name;
 
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    @Column(name = "cover_image_url", nullable = false)
+    private String coverImageUrl;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "is_saled", nullable = false)
+    private Boolean isSaled;
+
     @Column(name="created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private OffsetDateTime createdAt;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductSku> productSkuList = new ArrayList<>();
 
     public Product(){}
 
-    public Product(String name, LocalDateTime createdAt) {
+    public Product(String name, OffsetDateTime createdAt) {
         this.name = name;
         this.createdAt = createdAt;
     }
